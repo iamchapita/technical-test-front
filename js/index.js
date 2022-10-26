57a05217e71d49f5ce57ec49739ea3a0f4fe3086
iamchapita <alejandrom646@gmail.com>
2022-10-26 15:58:56 -0600

    Loading courser from js archive

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@
 @version: 0.1.0
 */
 
-function loadData(archive) {
+function loadProfilePicture(archive, idProfile=0) {
 	// Obteniendo el nombre del archivo para foto de perfil
-	var profilePicName = archive[0]["instructor"]["imagen"];
+	var profilePicName = archive[idProfile]["instructor"]["imagen"];
 	// Estableciendo la foto de perfil
 	var imgTag = document.getElementsByClassName("profilePicture");
 	imgTag = imgTag[0];
@@ -15,6 +15,29 @@ function loadData(archive) {
 
 	// Insertando foto de perfil, nombre y correo de los demas perfiles
 	var dropdown = document.getElementById("dropdownMenu");
+}
+
+function loadCourses(archive, idProfile=0) {
+	// Cargando los cursos del perfil seleccionado
+	var row = document.getElementById("row");
+    var child = row.lastElementChild;
+
+    while(child){
+        row.removeChild(child);
+        child = row.lastElementChild;
+    }
+
+	for (var i = 0; i < archive[idProfile]["clases"].length; i++) {
+		var name = archive[idProfile]["clases"][i]["nombreClase"];
+		var section = archive[idProfile]["clases"][i]["seccion"];
+
+		row.innerHTML +=
+			"<div class='col'><div class='card'><a href='#'><img src='/resources/pictures/na_january_35.jpg' class='card-img-top' alt='...'><div class='card-img-overlay'><h3 class='card-title'>" +
+			name +
+			"</h3><h5>" +
+			section +
+			"</h5></div></a><div class='card-body'><p class='card-text'></p><div class='footer'><div class='container'><span class='material-symbols-outlined'>folder</span><span class='material-symbols-outlined'>trending_up</span></div></div></div></div></div>";
+	}
 }
 
 function loadProfileList(archive) {
@@ -28,33 +51,25 @@ function loadProfileList(archive) {
 		dropdown.innerHTML +=
 			"<div class='profileList'><li class='option'> <img src='/resources/profile-pics/" +
 			image +
-			"'class='profilePicture'><a class='dropdown-item' href='#'>" +
+			"'class='profilePicture'><a class='dropdown-item' href='#' onclick='loadProfilePicture(classroom, "+i+"); loadCourses(classroom, "+i+");'>" +
 			name +
 			"<br>" +
 			email +
 			"</a></li></div>";
 	}
 
-	dropdown.innerHTML +=
-		"<div class='profileList'><li class='option'> <span class='material-symbols-outlined' id='personAdd'>person_add</span> <a class='dropdown-item' href='#'>Agregar Instructor</a></li></div>";
+    // var dropdownItems = document.getElementsByClassName('dropdown-item');
+
+    // for (var i = 0; i < dropdownItems.length; i++) {
+    //     dropdownItems[i].addEventListener("click", function () {
+    //         loadProfilePicture(archive, i);
+    //         loadCourses(archive, i);
+    //     });
+    // }
 
 	dropdown.innerHTML +=
-		"<div class='profileList'><li class='option'><a class='dropdown-item'>Privacy Police • Terms of Service</a></li></div>";
-}
+		"<div class='noProfileList'><li class='option'> <span class='material-symbols-outlined' id='personAdd'>person_add</span> <a class='dropdown-item' href='#'>Agregar Instructor</a></li></div>";
 
-function loadCourses(archive) {
-	// Cargando los cursos del perfil seleccionado
-	var row = document.getElementById("row");
-
-	for (var i = 0; i < archive[0]["clases"].length; i++) {
-		var name = archive[0]["clases"][i]["nombreClase"];
-		var section = archive[0]["clases"][i]["seccion"];
-
-		row.innerHTML +=
-			"<div class='col'><div class='card'><a href='#'><img src='/resources/pictures/na_january_35.jpg' class='card-img-top' alt='...'><div class='card-img-overlay'><h3 class='card-title'>" +
-			name +
-			"</h3><h5>" +
-			section +
-			"</h5></div></a><div class='card-body'><p class='card-text'></p><div class='footer'><div class='container'><span class='material-symbols-outlined'>folder</span><span class='material-symbols-outlined'>trending_up</span></div></div></div></div></div>";
-	}
+	dropdown.innerHTML +=
+		"<div class='noProfileList'><li class='option'><a class='dropdown-item'>Privacy Police • Terms of Service</a></li></div>";
 }
